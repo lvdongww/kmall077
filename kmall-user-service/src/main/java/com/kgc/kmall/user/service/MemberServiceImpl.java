d52e8a2a313984f192be7add3b61cca2f68e01b5
lvdong <2789501979@qq.com>
2020-12-15 16:14:05 +0800

    创建kmall-user-service,测试mybatis

---
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -0,0 +1,25 @@
+package com.kgc.kmall.user.service;
+
+import com.kgc.kmall.bean.Member;
+import com.kgc.kmall.service.MemberService;
+import com.kgc.kmall.user.mapper.MemberMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-12-15 16:10
+ */
+//@Component
+@Service
+public class MemberServiceImpl implements MemberService {
+    @Resource
+    MemberMapper memberMapper;
+    @Override
+    public List<Member> selectAllMember() {
+        return memberMapper.selectByExample(null);
+    }
+}
